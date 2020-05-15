275af37d3a810f89afe50cbb2c3cb2afb2af2d7f
buddi <kithinzi97@gmail.com>
2020-05-16 00:40:28 +0300

    todo and todos components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React,{ Component } from 'react';
+import Todos from './components/Todos.js';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Vision </h1>
-    </div>
-  );
+import './App.css';
+
+class App extends Component{
+
+	state = {
+		todos :
+			[
+				{
+					id:1,
+					title: "Meeting",
+					completed: false,
+				},
+				{
+					id:2,
+					title: "Clean",
+					completed: false,
+				},
+				{
+					id:3,
+					title: "Jog",
+					completed: false,
+				}
+			]
+		}
+
+	render(){
+	  return (
+	    <div className="App">
+	      <Todos todos={this.state.todos}/>
+	    </div>
+	  );
+	}
 }
 
 export default App;
